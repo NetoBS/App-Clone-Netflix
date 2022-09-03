88f02fa8d8f3ff8cb5d1ae2500a907894b387f1f
Neto Sousa <netosousa29@yahoo.com>
2022-09-03 20:18:48 -0300

    Criando login

---
+++ src/pages/login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import styles from './styles'
+
+const Login = () => {
+    return (
+        <View style={styles.bgDark}></View>
+    );
+};
+
+export default Login;
---
+++ src/pages/login/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    bgDark: {
+        
+            backgroundColor: '#1f1f38',
+            flex: 1,
+        
+    }
+})
+
+export default styles;
