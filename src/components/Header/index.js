ebda8ae6cfe087ebbee06fc2f362ea53ebf2655f
Neto Sousa <netosousa29@yahoo.com>
2022-09-05 17:24:55 -0300

    Criando o Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from 'react-native';
+
+import styles from './styles';
+
+const Header = () => {
+    return (
+        <>
+            <Text style={styles.textWhite}>Header</Text>
+        </>
+    )
+}
+
+export default Header;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Text, View } from "react-native";
 import styles from "./styles";
 
+import Header from '../../components/Header';
+
 const Home = () => {
     return (
         <View style={styles.container}>
+            <Header />
             <Text>Home</Text>
         </View>
     )
