f5a55cf693a2bf170a632801327956ff1b08e699
Neto Sousa <netosousa29@yahoo.com>
2022-09-06 15:16:51 -0300

    Criando a preview em home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { View, ScrollView, TouchableOpacity, FlatList, Image } from "react-native";
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from "./styles";
-import { Button } from "react-native-paper";
+import { Button, Title } from "react-native-paper";
 
 import Header from '../../components/Header';
 import Hero from '../../components/Hero';
 import ButtonVertical from "../../components/ButtonVertical";
 
+import LinearGradient from "react-native-linear-gradient";
+
 const Home = () => {
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <Header />
             <Hero />
             
@@ -22,7 +23,37 @@ const Home = () => {
                 </Button>
                 <ButtonVertical icon="information-outline" text="Saiba Mais" />
             </View>
-        </View>
+            
+            <View style={styles.previewContainer}>
+                <Title style={styles.previewTitle}>Prévias</Title>
+                <FlatList
+                    horizontal
+                    style={styles.flatListContainer}
+                    data={[1,2,3,4,5,6]}
+                    renderItem={({ item, index }) => (
+                        <TouchableOpacity key={index} style={{ marginLeft: index == 0 ? 20 : 0, marginRight: 10}}>
+                            <View style={styles.circle}>
+                               <Image 
+                                    style={styles.capa} 
+                                    source={{uri: 'https://i.imgur.com/EJyDFeY.jpg'}}
+                                />
+
+                               <Image 
+                                    resizeMode="contain"
+                                    style={styles.logo} 
+                                    source={{uri: 'http://i.imgur.com/4xN7wB8.png'}}
+                                /> 
+
+                                <LinearGradient
+                                    style={styles.gradient}
+                                    colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']} 
+                                />
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                />
+            </View>
+        </ScrollView>
     )
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -11,6 +11,46 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-evenly',
     },
+    flatListContainer: {
+        width: '100%',
+        height: 100,
+        marginTop: 10,
+        marginBottom: 50,
+    },
+    circle: {
+        backgroundColor: '#E50914',
+        padding: 2.5,
+        width: 90,
+        height: 90,
+        borderRadius: 90,
+    },
+    capa: {
+        height: 85,
+        width: 85,
+        borderRadius: 85,
+    },
+    logo: {
+        width: 90,
+        height: 45,
+        position: 'absolute',
+        zIndex: 10,
+        bottom: 10,
+        alignSelf: 'center',
+    },
+    gradient: {
+        width: '100%',
+        height: 60,
+        position: 'absolute',
+        zIndex: 9,
+        bottom: 0,
+    },
+    previewContainer: {
+        width: '100%',
+        marginTop: 50,
+    },
+    previewTitle: {
+        marginLeft: 20,
+    },
 });
 
 export default styles;
