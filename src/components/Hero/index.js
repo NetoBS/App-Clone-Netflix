917ec242309b8f9cc57638eb87865d30385870a2
Neto Sousa <netosousa29@yahoo.com>
2022-09-05 22:23:31 -0300

    Adicionando capa e logos

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { Text, ImageBackground, View, Image } from 'react-native';
 
 import styles from './styles';
 
 const Hero = () => {
     return (
-        <>
-            <Text style={{color: '#fff'}}>Hero</Text>
-        </>
-    )
+        <ImageBackground 
+            style={styles.hero}
+            source={{ uri: 'https://i.imgur.com/EJyDFeY.jpg'}}>
+            
+            <Image 
+                style={styles.logo}
+                resizeMode="contain"
+                source={{ uri: 'http://i.imgur.com/4xN7wB8.png'}}
+            />
+
+            <View style={styles.containerTop10}>
+                <Image 
+                    style={styles.top10Badge} 
+                    resizeMode="contain"
+                    source={require('../../assets/badge-top-10.png')}
+                />
+                <Text style={styles.top10Text}>Top 1 de hoje no Brasil</Text>
+            </View>
+            <LinearGradient
+                style={styles.gradient}
+                colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']} 
+            />
+            
+        </ ImageBackground>
+        
+        )
 };
 
 export default Hero;
--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,5 +1,43 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    hero: {
+        height: 400,
+        width: '100%',
+    },
+    logo: {
+        marginTop: 200,
+        alignSelf: 'center',
+        width: 300,
+        height: 150,
+        position: "absolute",
+        zIndex: 10,
+    },
+    containerTop10: {
+        position: 'absolute',
+        zIndex: 10,
+        bottom: 20,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100%',
+        alignItems: 'center',
+    },
+    top10Badge: {
+        width: 20,
+        height: 20,
+        marginRight: 10,
+    },
+    top10Text: {
+        color: '#fff',
+        fontSize: 15,
+    },
+    gradient: {
+        width: '100%',
+        height: 150,
+        position: 'absolute',
+        zIndex: 9,
+        bottom: 0,
+    },
+});
 
 export default styles;
