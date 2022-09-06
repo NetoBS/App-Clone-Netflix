0a1b08408242a881107593e619b13e1c7efb1dca
Neto Sousa <netosousa29@yahoo.com>
2022-09-06 11:37:20 -0300

    Adicionando Botoes e icons na  home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from "./styles";
+import { Button } from "react-native-paper";
 
 import Header from '../../components/Header';
 import Hero from '../../components/Hero';
@@ -10,7 +13,22 @@ const Home = () => {
         <View style={styles.container}>
             <Header />
             <Hero />
-            <Text>Home</Text>
+            
+            <View style={styles.menuHeader}>
+                <TouchableOpacity style={styles.menuButton}>
+                    <Icon name="plus" color="#fff" size={20} />
+                    <Text style={styles.buttonText}>Minha Lista</Text>
+                </TouchableOpacity>
+
+                <Button icon="play" mode="contained" color="#fff" uppercase={false}>
+                    Assistir
+                </Button>
+
+                <TouchableOpacity style={styles.menuButton}>
+                    <Icon name="information-outline" color="#fff" size={20} />
+                    <Text style={styles.buttonText}>Saiba Mais</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,6 +5,19 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#000',
     },
+    menuHeader: {
+        width: '100%',
+        height: 38,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    buttonText: {
+        color: '#fff',
+    },
+    menuButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
 
 export default styles;
