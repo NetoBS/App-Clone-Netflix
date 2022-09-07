f7c17f7607bd617933831613cd345257786969c5
Neto Sousa <netosousa29@yahoo.com>
2022-09-07 16:30:12 -0300

    Finalizando as sections

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
-import styles from './styles';
+import { Text, View, FlatList, TouchableOpacity, ImageBackground, Image } from 'react-native';
+import { Title } from 'react-native-paper';
 
-import { Text } from 'react-native';
+import styles from './styles';
 
 const Sections = () => {
     return (
-        <Text style={{color: '#fff'}}>Seção</ Text>
+        <View style={styles.container}>
+            <Title style={styles.sectionsTitle}>Nome da Seção</ Title>
+
+            <FlatList
+                style={styles.list}
+                horizontal
+                data={[1, 2, 3, 4, 5, 6,]}
+                renderItem={({ item, index }) => (
+                    <TouchableOpacity key={index}>
+                        <ImageBackground 
+                            style={[styles.capa, {marginRight: 10, marginLeft: index == 0 ? 20 : 0},]}
+                            source={{ uri: 'https://i.imgur.com/EJyDFeY.jpg' }}
+                        >
+                            <Image 
+                                resizeMode="contain" 
+                                style={styles.logo} 
+                                source={{ uri: 'http://i.imgur.com/4xN7wB8.png' }} 
+                            />
+                        </ImageBackground>
+                    </TouchableOpacity>
+                )}
+            />
+        </View>
     );
 };
 
--- src/components/Sections/styles.js
+++ src/components/Sections/styles.js
@@ -1,9 +1,32 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    SectionsContainer: {
-        
-    }
+    container: {
+        width: '100%',
+    },
+    sectionsTitle: {
+        marginLeft: 20,
+    },
+    list: {
+        width: '100%',
+        height: 180,
+        marginTop: 10,
+        marginBottom: 30,
+    },
+    capa: {
+        width: 120,
+        height: 180,
+        borderRadius: 4,
+        overflow: 'hidden',
+    },
+    logo: {
+        width: 100,
+        height: 45,
+        position: 'absolute',
+        zIndex: 10,
+        bottom: 20,
+        alignSelf: 'center',
+    },
 });
 
 export default styles;
