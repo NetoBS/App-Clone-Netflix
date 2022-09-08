1e9625a16670f7fa89cf0bd90848397f0213e74d
Neto Sousa <netosousa29@yahoo.com>
2022-09-08 16:44:23 -0300

    Adicionando capa dos filmes na page movie

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 
-import { Text } from "react-native";
+import { ScrollView, ImageBackground, View } from 'react-native';
+import { Title, Button } from 'react-native-paper';
+
+import styles from "./styles";
 
 const Movie = () => {
     return (
-        <Text>Filme</Text>
+        <ScrollView style={styles.container}>
+            <ImageBackground source={{ uri: 'https://images2.alphacoders.com/551/551538.jpg' }} style={styles.hero} />
+            <View style={styles.containerPadding}>
+                <Title>Nome do Filme</Title>
+
+                <Button style={styles.buttonPlay} icon="play" mode="contained" color="#fff" uppercase={false}>
+                    Assistir
+                </Button>
+            </View>
+        </ScrollView>
     );
 };
 
--- src/pages/Movie/styles.js
+++ src/pages/Movie/styles.js
@@ -1,5 +1,17 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+    },
+    hero: {
+        height: 200,
+        width: '100%',
+    },
+    containerPadding: {
+        padding: 20,
+    },
+});
 
 export default styles;
