999151c7b87e9ebcdfcb7dc6cdaf81d28968b470
Neto Sousa <netosousa29@yahoo.com>
2022-09-08 20:21:22 -0300

    Finalizando a page de filme

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,21 +1,53 @@
 import React from "react";
 
 import { ScrollView, ImageBackground, View } from 'react-native';
-import { Title, Button } from 'react-native-paper';
+import { Title, Button, Paragraph, Caption } from 'react-native-paper';
+
+import ButtonVertical from "../../components/ButtonVertical";
+import Sections from "../../components/Sections";
 
 import styles from "./styles";
 
 const Movie = () => {
     return (
         <ScrollView style={styles.container}>
-            <ImageBackground source={{ uri: 'https://images2.alphacoders.com/551/551538.jpg' }} style={styles.hero} />
+            <ImageBackground source={{ uri: 'https://images7.alphacoders.com/113/1134920.jpg' }} style={styles.hero} />
             <View style={styles.containerPadding}>
                 <Title>Nome do Filme</Title>
 
                 <Button style={styles.buttonPlay} icon="play" mode="contained" color="#fff" uppercase={false}>
                     Assistir
                 </Button>
+
+                <Paragraph>
+                    Após Thanos eliminar metade das criaturas vivas de todo o universo, 
+                    os heróis sobreviventes precisam lidar com a dor da perda de amigos e seus entes queridos.
+                </Paragraph>
+
+                <Caption style={styles.captionInfos}>
+                    Elenco:{' '}
+                        <Caption style={styles.captionWhite}>
+                            Downey Jr., Hemsworth, Evans, Pratt, Rudd, Cumberbatch, Holland.
+                        </Caption>{' '}
+                    Gênero:{' '}
+                        <Caption style={styles.captionWhite}>
+                            Ação, Super-herói, Aventura, Ficção científicaFantasia, The Avengers/Genres. 
+                        </Caption>{' '}
+                    Cenas e momentos:{' '}
+                        <Caption style={styles.captionWhite}>
+                            The Avengers/Genres. 
+                        </Caption>
+                </Caption>
+
+                <View style={styles.menuIcons}>
+                    <ButtonVertical icon="plus" text="Minha Lista" />
+                    <ButtonVertical icon="thumb-up" text="Classifique" />
+                    <ButtonVertical icon="send" text="Compartilhe" />
+                    <ButtonVertical icon="download" text="Baixar" />
+                </View>
             </View>
+
+            <Sections hasTopBorder />
         </ScrollView>
     );
 };
--- src/pages/Movie/styles.js
+++ src/pages/Movie/styles.js
@@ -12,6 +12,22 @@ const styles = StyleSheet.create({
     containerPadding: {
         padding: 20,
     },
+    buttonPlay: {
+        marginVertical: 20,
+    },
+    captionInfos: {
+        marginTop: 20,
+    },
+    captionWhite: {
+        color: '#fff',
+    },
+    menuIcons: {
+        width: '100%',
+        height: 38,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 20,
+    },
 });
 
 export default styles;
